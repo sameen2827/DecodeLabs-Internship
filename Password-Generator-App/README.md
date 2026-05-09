f77bcb14d98c826220fdee637be522d9f2757972
Sameen <sameenfatima2827@gmail.com>
2026-05-09 15:09:51 +0500

    Added Password Generator App

---
+++ Password-Generator-App/README.md
@@ -0,0 +1,14 @@
+# Random Password Generator
+
+A Python-based command-line application that generates secure and random passwords using letters, numbers, and special characters.
+
+## Features
+-Generate random passwords
+-User-defined password length
+-includes letters, digits, and symbols
+-Simple and secure CLI interface
+
+## Tech Stack
+-Python
+-random module
+-string module
--- Password-Generator-App/password-generator.py
+++ Password-Generator-App/password-generator.py
@@ -15,19 +15,19 @@ def main():
         length = int(input("\nEnter password length: "))
 
         if length < 4:
-            print("Password length should be atleast 4.")
+            print("Password length should be at least 4.")
             return
 
     except ValueError:
-        print("Invalid input, please enter a valid number.")
+        print("Invalid input! Please enter a number.")
         return
 
-    
+    # Character Sets
     letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
 
-    
+    # Password Types
     simple_password = generate_password(length, letters)
 
     numeric_password = generate_password(length, digits)
@@ -42,21 +42,20 @@ def main():
         letters + digits
     )
 
-   
+    # Output
     print("\n" + "=" * 45)
-    print(" Generated Passwords")
-    print("=" *45)
+    print("           GENERATED PASSWORDS")
+    print("=" * 45)
 
-    print(f"\n1. Simple Password  :{simple_password}")
-    print(f"2. Numeric Password  :{numeric_password}")
-    print(f"3. Alphanumeric Password :{alphanumeric_password}")
-    print(f"4. Strong Password :{strong_password}")
+    print(f"\n1. Simple Password       : {simple_password}")
+    print(f"2. Numeric Password      : {numeric_password}")
+    print(f"3. Alphanumeric Password : {alphanumeric_password}")
+    print(f"4. Strong Password       : {strong_password}")
 
     print("\n" + "=" * 45)
-    print(" Passwords Generated Successfully!")
-    print("=" * 45 + "\n")
+    print(" Password generation completed successfully!")
+    print("=" * 45)
 
 
 if __name__ == "__main__":
     main()
-    
